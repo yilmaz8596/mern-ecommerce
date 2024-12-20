e24a5260ae95d5a5fe1f4a06392e771ffd60aaf8
yilmaz8596 <yilmaz8596@gmail.com>
2024-12-20 12:25:58 +0300

    setup backend and create database

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = async (req, res) => {
+  res.send("Register");
+};
+
+export const login = async (req, res) => {
+  res.send("Login");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout");
+};
---
+++ api/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
