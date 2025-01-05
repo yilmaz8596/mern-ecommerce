3b7f0edd12b12318e009f83e2ae6cf22dbdce686
Fırat Yılmaz <yilmaz8596@gmail.com>
2025-01-05 12:58:34 +0300

    fix bug

--- api/server.js
+++ api/server.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
