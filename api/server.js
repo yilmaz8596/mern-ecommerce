bddee7af9f17dcf50c8379354a071d48f2ecf1c7
Fırat Yılmaz <yilmaz8596@gmail.com>
2025-01-05 12:37:59 +0300

    prepare for deployment

--- api/server.js
+++ api/server.js
@@ -9,12 +9,15 @@ import couponRoutes from "./routes/coupon.routes.js";
 import paymentRoutes from "./routes/payment.routes.js";
 import analyticsRoutes from "./routes/analytics.routes.js";
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
 connectDB();
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -42,6 +45,14 @@ app.use((err, req, res, next) => {
     message: errMessage,
   });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
